9ef4d09c306aad752e01594b8d60a8fb188ec464
Kirkemann <kirkemann35@gmail.com>
2019-09-17 12:32:59 +0200

    Rettet img i navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
     return ( 
         <div className="container">
         <nav className="navbar navbar-expand-lg navbar-light bg-transparent">
-            <a className="navbar-brand" href="/">hej<img src={RunInc} alt="runins"/>hej</a>
+            <a className="img navbar-brand" href="/"><img src={RunInc} alt="runins"/></a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
